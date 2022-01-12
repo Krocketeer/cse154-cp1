9e93e446d9e644c1e6da482fac857d20b330a914
Krocketeer <Krocketeer@github.com>
2022-01-12 03:06:54 -0800

    Creative Project 1 initial upload

---
+++ src/js/About.js
@@ -0,0 +1,28 @@
+/**
+ * Kenny "Ackerson" Le
+ * CSE 154 AF, Winter 2022
+ * TA: Ludvig Liljenberg, Marina Wooden
+ * 1/5/22
+ * Creative Project #1
+ * Description: Javascript file to control accordion element on about page
+ */
+
+let row = document.getElementsByClassName('accordion-header');
+let text = document.getElementsByClassName('row-text');
+let button = document.getElementsByClassName('accordion-button');
+
+for (let i = 0; i < row.length; i++) {
+    row[i].addEventListener('click', function() {
+        if (text[i].style.maxHeight) {
+            row[i].classList.remove('active');
+            text[i].classList.remove('active');
+            text[i].style.maxHeight = null;
+            button[i].setAttribute('src', 'images/components/plus.png');
+        } else {
+            row[i].classList.add('active');
+            text[i].classList.add('active');
+            text[i].style.maxHeight = text[i].scrollHeight + 'px';
+            button[i].setAttribute('src', 'images/components/subtract.png');
+        }
+    })
+}
