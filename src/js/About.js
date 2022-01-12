ada86014e4841e81d905302cdf3dbea75720b8b9
Krocketeer <Krocketeer@github.com>
2022-01-12 12:01:26 -0800

    More linter fixes in HTML and About.jsJS rework

--- src/js/About.js
+++ src/js/About.js
@@ -7,26 +7,29 @@
  * Description: Javascript file to control accordion element on about page
  */
 
-window.addEventListener('load', function() {
-    console.log('page is fully loaded');
+"use strict";
+(function() {
+    window.addEventListener('load', init);
 
-    let row = document.getElementsByClassName('accordion-header');
-    let text = document.getElementsByClassName('row-text');
-    let button = document.getElementsByClassName('accordion-button');
+    function init() {
+        let row = document.getElementsByClassName('accordion-header');
+        let text = document.getElementsByClassName('row-text');
+        let button = document.getElementsByClassName('accordion-button');
 
-    for (let i = 0; i < row.length; i++) {
-        row[i].addEventListener('click', function() {
-            if (text[i].style.maxHeight) {
-                row[i].classList.remove('active');
-                text[i].classList.remove('active');
-                text[i].style.maxHeight = null;
-                button[i].setAttribute('src', 'images/components/plus.png');
-            } else {
-                row[i].classList.add('active');
-                text[i].classList.add('active');
-                text[i].style.maxHeight = text[i].scrollHeight + 'px';
-                button[i].setAttribute('src', 'images/components/subtract.png');
-            }
-        })
+        for (let i = 0; i < row.length; i++) {
+            row[i].addEventListener('click', function() {
+                if (text[i].style.maxHeight) {
+                    row[i].classList.remove('active');
+                    text[i].classList.remove('active');
+                    text[i].style.maxHeight = null;
+                    button[i].setAttribute('src', 'images/components/plus.png');
+                } else {
+                    row[i].classList.add('active');
+                    text[i].classList.add('active');
+                    text[i].style.maxHeight = text[i].scrollHeight + 'px';
+                    button[i].setAttribute('src', 'images/components/subtract.png');
+                }
+            })
+        }
     }
-});
+})();
